7eac127d9f3207d2495821c9d3aa18ca6ac0dc6a
Jean Luis Lopez <jeanluis.jl19@gmail.com>
2018-07-14 11:26:32 -0400

    Add configurations for staging

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -7,7 +7,7 @@ from .production import *
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='fufOOEOpzM7mrd12EBzilmerFCkamtglCJtddghKiFHtJn201IIZrL0Zk5vpgPbb')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['comunidad.do', 'comunidaddo.herokuapp.com'])
 
